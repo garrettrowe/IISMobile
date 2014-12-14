f85f11a4a37ce2a1baac00bf0809e11530b9d837
Garrett Rowe <garrettrowe@Garretts-MacBook.local>
2014-12-14 10:48:49 -0800

    Initial Commit

---
+++ Bluemix Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bluemix Viewer
+//
+//  Created by Garrett Rowe on 12/14/14.
+//  Copyright (c) 2014 Garrett Rowe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
